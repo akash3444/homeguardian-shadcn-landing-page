ac7fbbadcc3b9816a580a4b003a338fe6ee5c8d7
akash3444 <akashmoradiya3444@gmail.com>
2025-10-15 10:12:49 +0530

    feat: enhance hero component with background pattern

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,55 @@ export function Hero() {
       </div>
 
       <div className="mt-20 aspect-video border rounded-lg bg-muted p-2">
-        <div className="bg-background h-full w-full rounded-md border"></div>
+        <div className="bg-background h-full w-full rounded border relative">
+          <div
+            className="absolute inset-0 z-0"
+            style={{
+              backgroundImage: `
+        linear-gradient(to right, var(--border) 1px, transparent 1px),
+        linear-gradient(to bottom, var(--border) 1px, transparent 1px)
+      `,
+              backgroundSize: "20px 20px",
+              backgroundPosition: "0 0, 0 0",
+              maskImage: `
+       repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+          radial-gradient(ellipse 60% 60% at 50% 50%, #000 30%, transparent 70%)
+      `,
+              WebkitMaskImage: `
+ repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+          radial-gradient(ellipse 60% 60% at 50% 50%, #000 30%, transparent 70%)
+      `,
+              maskComposite: "intersect",
+              WebkitMaskComposite: "source-in",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
